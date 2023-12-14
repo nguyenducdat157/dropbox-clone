54b555b4ccd846c4be29753d3f6f56a837f1f316
hkhieudev <hk_hieu@lermo.co.jp>
2023-12-14 17:34:30 +0700

    done function drop

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import Dropzone from "@/components/Dropzone"
+import { auth } from "@clerk/nextjs"
+
+export default function Dashboard() {
+    const { userId } = auth()
+  return (
+    <div>
+        <Dropzone />
+    </div>
+  )
+}
